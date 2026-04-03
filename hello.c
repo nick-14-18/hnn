a102d8a5d2d7ab448ae8db0dde821cd5a8eb30f5
Vikas Kumar <vikaskumar90655@gmail.com>
2026-04-04 03:00:41 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("ENTER THE FIRST NUMBER=");
+    scanf("%d",&a);
+    printf("ENTER THE SECOND NUMBER=");
+    scanf("%d",&b);
+    return 0;
+}
